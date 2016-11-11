72a63d9550b48b0ef988fb16179c5cce47f43d14
Ashton Fillery <ashtonfillery@gmail.com>
2016-11-10 17:45:17 -0800

    yy

---
+++ Employees/Employees/Employye Info.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+   public class Employye_Info
+    {
+        private int employeeID;
+        private string EmployeeName;
+        private string EmployeeAddress;
+        private int employeeZipCode;
+        private DateTime employeeHireDate;
+        private DateTime employeeTerminationDate;
+public int EmployeeID
+        {
+            get
+            {
+                return employeeID;
+            }
+
+            set
+            {
+                employeeID = value;
+            }
+        }
+
+        public string EmployeeName1
+        {
+            get
+            {
+                return EmployeeName;
+            }
+
+            set
+            {
+                EmployeeName = value;
+            }
+        }
+
+        public string EmployeeAddress1
+        {
+            get
+            {
+                return EmployeeAddress;
+            }
+
+            set
+            {
+                EmployeeAddress = value;
+            }
+        }
+
+        public int EmployeeZipCode
+        {
+            get
+            {
+                return employeeZipCode;
+            }
+
+            set
+            {
+                employeeZipCode = value;
+            }
+        }
+
+        public DateTime EmployeeHireDate
+        {
+            get
+            {
+                return employeeHireDate;
+            }
+
+            set
+            {
+                employeeHireDate = value;
+            }
+        }
+
+        public DateTime EmployeeTerminationDate
+        {
+            get
+            {
+                return employeeTerminationDate;
+            }
+
+            set
+            {
+                employeeTerminationDate = value;
+            }
+        }
+    }
+}
---
+++ Employees/Employees/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Employees
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Employye_Info> EmployeeInfo = new List<Employye_Info>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            EmployeeList.ItemsSource = EmployeeInfo;
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            Employye_Info newEmp = new Employye_Info();
+            newEmp.EmployeeName1 = textBox_name.Text;
+            string EmpId = EmployyeIDBox.Text;
+            newEmp.EmployeeID = Convert.ToInt32(EmpId);
+            newEmp.EmployeeAddress1 = textbox_Address.Text;
+            string EmpZip = textBox_Zip.Text;
+            newEmp.EmployeeZipCode = Convert.ToInt32(EmpZip);
+            string EmpHire = textBox_HireDate.Text;
+            newEmp.EmployeeHireDate = Convert.ToDateTime(EmpHire);
+            string EmpTerm = textBox_TermDate.Text;
+            newEmp.EmployeeTerminationDate = Convert.ToDateTime(EmpTerm);
+
+            EmployeeInfo.Add(newEmp);
+            EmployeeList.ItemsSource = null;
+            EmployeeList.ItemsSource = EmployeeInfo;
+        }
+    }
+}
